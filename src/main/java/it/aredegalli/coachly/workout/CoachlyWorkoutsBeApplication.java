88c0d75e163c734caeedc2bab17ba11d471cc076
AEndrix <redegalli.andrea03@gmail.com>
2026-03-14 02:00:38 +0100

    fix: widen component scan for commons beans

--- src/main/java/it/aredegalli/coachly/workout/CoachlyWorkoutsBeApplication.java
+++ src/main/java/it/aredegalli/coachly/workout/CoachlyWorkoutsBeApplication.java
@@ -3,7 +3,7 @@ package it.aredegalli.coachly.workout;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "it.aredegalli.coachly")
 public class CoachlyWorkoutsBeApplication {
 
 	public static void main(String[] args) {
